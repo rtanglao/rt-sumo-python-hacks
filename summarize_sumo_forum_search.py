47fe456b5ebed51c78fbf6b457855f527b06f3d3
Roland Tanglao <roland@rolandtanglao.com>
2013-12-11 14:24:20 -0800

    add exception handling to fix 404s

--- summarize_sumo_forum_search.py
+++ summarize_sumo_forum_search.py
@@ -20,27 +20,28 @@ soup = BeautifulSoup(html_search_page)
 all_h3 = soup.find_all('h3')
 for child in soup.find_all('h3'):
     for l in child.children:
-          rel_link = l.get('href')
-          title = l.contents
-          url = "https://support.mozilla.org/en-US" + rel_link
-          response = urllib2.urlopen(url)
-          html = response.read()
-          s2 = BeautifulSoup(html)
-          main_content  = s2.findAll("div", { "class" : "main-content" })
-          if  type (main_content[0].p.contents[0]) is bs4.element.NavigableString:
-              first_p  = main_content[0].p.contents[0].rstrip()
-          else:
-              if type(main_content[0].p.contents[0].contents) is list:
-                  first_p = main_content[0].p.contents[0].contents[0].rstrip()
-              else:
-                  first_p  = main_content[0].p.contents[0].contents.rstrip()
-          first_75 = first_p[:75] + (first_p[75:] and u'..')
-          time_str = s2.find_all('time')[0]['datetime']
-          t2 = dateutil.parser.parse(time_str)
-          date = t2.strftime("%a %b %d %Y %I:%m %p")
-          #print "URL:", url
-          #print "title:", title[0]
-          #print "first 75:", first_75
-          print >> sys.stderr, 'first75:', first_75, 'title[0]:', title[0]
-          print '1. **%s** [%s](%s "%s")' % (date, title[0], url, first_75)
+        rel_link = l.get('href')
+        title = l.contents
+        try:
+            url = "https://support.mozilla.org/en-US" + rel_link
+            print >> sys.stderr, 'url of question:', url
+            response = urllib2.urlopen(url)
+            html = response.read()
+            s2 = BeautifulSoup(html)
+            main_content  = s2.findAll("div", { "class" : "main-content" })
+            if type (main_content[0].p.contents[0]) is bs4.element.NavigableString:
+                first_p  = main_content[0].p.contents[0].rstrip()
+            else:
+                if type(main_content[0].p.contents[0].contents) is list:
+                    first_p = main_content[0].p.contents[0].contents[0].rstrip()
+                else:
+                    first_p  = main_content[0].p.contents[0].contents.rstrip()
+            first_75 = first_p[:75] + (first_p[75:] and u'..')
+            time_str = s2.find_all('time')[0]['datetime']
+            t2 = dateutil.parser.parse(time_str)
+            date = t2.strftime("%a %b %d %Y %I:%m %p")
+            print >> sys.stderr, 'first75:', first_75, 'title[0]:', title[0]
+            print '1. **%s** [%s](%s "%s")' % (date, title[0], url, first_75)
+        except:
+            pass
 
