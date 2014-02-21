ed154a4306315a0645901d3b18c5b34a38d72af6
Roland Tanglao <roland@rolandtanglao.com>
2014-02-21 15:44:26 -0800

    sometimes you don't want to edit

---
+++ open-tb-non-edit-slug-from-stdin-or-file.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python
+# coding: utf-8
+import webbrowser
+import sys
+for line in sys.stdin:
+  print >> sys.stderr, "slug", line
+  line = line.lower().split()
+  slug = line[0] 
+  webbrowser.open_new_tab("https://support.mozilla.org/en-US/kb/"+slug)
